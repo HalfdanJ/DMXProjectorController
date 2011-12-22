5c9c3506ac35f2a574fbd1dd309d59344b5ce03b
Jonas Jongejan <jonas@halfdanj.dk>
2011-12-22 12:44:49 +0100

    Fixed bug with only first 13 channels working. But channel 256-512 won't work

--- ActionLoop.ino
+++ ActionLoop.ino
@@ -1,7 +1,6 @@
 void action() { 
 
-  receiveDmxTimeout = 3000;
-
+  receiveDmxTimeout = 32000;
   /*********** Put what you want the code to do with the values (dmxvalue) here *************
    * example code: print out the received values to the serial port, and set PWM pins 5 and 6 
    to the first two values received.  You can take this code out and put your own in.*/
@@ -12,57 +11,83 @@ void action() {
   /* analogWrite(5, dmxvalue[0]);
    analogWrite(6, dmxvalue[1]);*/
   //end example code
-  
+  /*
   mySerial.print(readAddress());
-  mySerial.print("  ");
-  mySerial.print(dmxvalue[2+readAddress()]);
-  mySerial.print("  ");
-  mySerial.print(dmxvalue[2+readAddress()+1]);
-  mySerial.print("  ");
-  mySerial.print(dmxvalue[2+readAddress()+2]);
-  mySerial.print(" \n ");
-/*
+   mySerial.print("  ");
+   mySerial.print(dmxvalue[2+readAddress()]);
+   mySerial.print("  ");
+   mySerial.print(dmxvalue[2+readAddress()+1]);
+   mySerial.print("  ");
+   mySerial.print(dmxvalue[2+readAddress()+2]);
+   mySerial.print(" \n ");*/
+  fastBlink = false;
+
   if(dmxvalue[2+readAddress()] > 127) {
-    //Video Mute
-    digitalWrite(13, HIGH);  
-    mySerial.print('C');
-    mySerial.print('0');
-    mySerial.print('D');
-    mySerial.print('\n');
-  } else {
-    //Video Mute Off
-    digitalWrite(13, HIGH);  
-    mySerial.print('C');
-    mySerial.print('0');
-    mySerial.print('E');
-    mySerial.print('\n');
+    fastBlink = true;
+    if(lastShutterSend == false){
+      lastShutterSend = true;
+
+      //Video Mute
+      mySerial.print('C');
+      mySerial.print('0');
+      mySerial.print('D');
+      mySerial.print('\n');
+    }
+  } 
+  else {
+    if(lastShutterSend == true){
+      lastShutterSend = false;
+
+      //Video Mute Off
+      mySerial.print('C');
+      mySerial.print('0');
+      mySerial.print('E');
+      mySerial.print('\n');
+    }
   }
-    
+
   if(dmxvalue[2+readAddress()+1] > 127) {
-    //Turn on
-    digitalWrite(13, HIGH);
-    mySerial.print('C');
-    mySerial.print('0');
-    mySerial.print('0');
-    mySerial.print('\n');
+    fastBlink = true;
+    if(lastOnSend == false){
+      lastOnSend = true;
+
+      //Turn on
+      mySerial.print('C');
+      mySerial.print('0');
+      mySerial.print('0');
+      mySerial.print('\n');
+    }
+  } 
+  else {
+    lastOnSend = false;
   }
   if(dmxvalue[2+readAddress()+2] > 127) {
-    //Turn off
-    digitalWrite(13, HIGH);
-    mySerial.print('C');
-    mySerial.print('0');
-    mySerial.print('1');
-    mySerial.print('\n');
+    fastBlink = true;
+    if(lastOffSend == false){
+      lastOffSend = true;
+
+      //Turn off
+      mySerial.print('C');
+      mySerial.print('0');
+      mySerial.print('1');
+      mySerial.print('\n');
+    }
   }
   else {
-    digitalWrite(13, LOW);
+    lastOffSend = false;
   }
-  
-  */
+
+
 
 
   return;  //go back to loop()
 } //end action() loop
+
+
+
+
+
+
 
 
 
--- DMXProjectorController.ino
+++ DMXProjectorController.ino
@@ -1,37 +1,42 @@
 /***********************************************************
- * DMX-512 Reception                                        *
- * Developed by Max Pierson                                 *
- * Version Rev15 9 Oct 2010                                 *
- * Released under the WTFPL license, although I would       *
- * appreciate Attribution and Share-Alike                   *
- * See blog.wingedvictorydesign.com for the latest version. *
+ * DMXProjectorController                                   *
+ * Developed by HalfdanJ.dk                                 *
  ************************************************************/
 
-/******************************* Addressing variable declarations *****************************/
 #include <SoftwareSerial.h>
+#include "EEPROM.h"
 
-#define rxPin 8
-#define txPin 9
-#define ledPin 13
+//Pin assignment
+#define SerialRxPin 8
+#define SerialTxPin 9
+#define ledPin 6
 
+//LED Status
 int receiveDmxTimeout = 0;
 int receiveDmxBlinkTimer = 0;
-int ledBaseValue = 500;
+boolean blinkState = false;
+boolean fastBlink = false;
 
-SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
 
-#include "EEPROM.h"
-#define NUMBER_OF_CHANNELS 3+10
-//the number of channels we want to receive (8 by default).
+//Send states
+boolean lastShutterSend = false;
+boolean lastOnSend = false;
+boolean lastOffSend = false;
+
+//Serial
+SoftwareSerial mySerial =  SoftwareSerial(SerialRxPin, SerialTxPin);
+
+//DMX Setup
+#define NUMBER_OF_CHANNELS 255 //512
 
 #define SWITCH_PIN_0 11 //the pin number of our "0" switch
 #define SWITCH_PIN_1 12 //the pin number of our "1" switch
-unsigned int dmxaddress = 1;
-/* The dmx address we will be listening to.  The value of this will be set in the Addressing()
- *  function and read from EEPROM addresses 510 and 511.
- 
-/******************************* MAX485 variable declarations *****************************/
 
+//DMX Address
+unsigned int dmxaddress = 1;
+
+
+//MAX485 variable declarations
 #define RECEIVER_OUTPUT_ENABLE 2
 /* receiver output enable (pin2) on the max485.
  *  will be left low to set the max485 to receive data. */
@@ -40,11 +45,11 @@ unsigned int dmxaddress = 1;
 /* driver output enable (pin3) on the max485.
  *  will left low to disable driver output. */
 
-#define RX_PIN 0   // serial receive pin, which takes the incoming data from the MAX485.
-#define TX_PIN 1   // serial transmission pin
+#define DMXRxPin 0   // serial receive pin, which takes the incoming data from the MAX485.
+#define DMXTxPin 1   // serial transmission pin
 
-/******************************* DMX variable declarations ********************************/
 
+//DMX variable declarations
 volatile byte i = 0;              //dummy variable for dmxvalue[]
 volatile byte dmxreceived = 0;    //the latest received value
 volatile unsigned int dmxcurrent = 0;     //counter variable that is incremented every time we receive a value.
@@ -55,19 +60,19 @@ volatile boolean dmxnewvalue = false;
 /*  set to 1 when updated dmx values are received
  *  (even if they are the same values as the last time). */
 
-/******************************* Timer2 variable declarations *****************************/
-
+//Timer2 variable declarations
 volatile byte zerocounter = 0;
 /* a counter to hold the number of zeros received in sequence on the serial receive pin.
  *  When we've received a minimum of 11 zeros in a row, we must be in a break.  */
 
+
 void setup() {
-  pinMode(rxPin, INPUT);
-  pinMode(txPin, OUTPUT);
+  pinMode(SerialRxPin, INPUT);
+  pinMode(SerialTxPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(6, OUTPUT);
   mySerial.begin(9600);
-  
+
   pinMode(14, OUTPUT);
   pinMode(15, OUTPUT);
   pinMode(16, OUTPUT);
@@ -81,9 +86,9 @@ void setup() {
   pinMode(RECEIVER_OUTPUT_ENABLE, OUTPUT);
   pinMode(DRIVER_OUTPUT_ENABLE, OUTPUT);
   digitalWrite(RECEIVER_OUTPUT_ENABLE, LOW);
-  digitalWrite(DRIVER_OUTPUT_ENABLE, LOW);    //sets pins 3 and 4 to low to enable reciever mode on the MAX485.
+  digitalWrite(DRIVER_OUTPUT_ENABLE, LOW);
 
-  pinMode(RX_PIN, INPUT);  //sets serial pin to receive data
+  pinMode(DMXRxPin, INPUT);  //sets serial pin to receive data
 
   /******************************* Addressing subroutine *********************************/
 
@@ -91,16 +96,6 @@ void setup() {
   digitalWrite(SWITCH_PIN_0, HIGH);       //turns on the internal pull-up resistor for '0' switch pin
   pinMode(SWITCH_PIN_1, INPUT);           //sets pin for '1' switch to input
   digitalWrite(SWITCH_PIN_1, HIGH);       //turns on the internal pull-up resistor for '1' switch pin
-
-  /* Call the addressing subroutine.  Three behaviors are possible:
-   *  1. Neither switch is pressed, in which case the value previously stored in EEPROM
-   *  510 and 511 is recalled,
-   *  2. Both switches are pressed, in which case the address is reset to 1.
-   *  3. Either switch is pressed (but not both), in which case the new address may
-   *  be entered by the user.
-   */
-  //set this equal to a constant value if you just want to hardcode the address.
-  // dmxaddress = Addressing();
 
   /******************************* USART configuration ************************************/
 
@@ -142,9 +137,9 @@ void setup() {
 
 void loop()  {
   // the processor gets parked here while the ISRs are doing their thing. 
-    
+
   if (dmxnewvalue == 1) {    //when a new set of values are received, jump to action loop...
-    action();
+    action(); //Here is the fun!
     dmxnewvalue = 0;
     dmxcurrent = 0;
     zerocounter = 0;      //and then when finished reset variables and enable timer2 interrupt
@@ -152,22 +147,31 @@ void loop()  {
     bitSet(TIMSK2, OCIE2A);    //Enable Timer/Counter2 Output Compare Match A Interrupt
   }
 
-
+  //As long as DMX is received blink quickly, if command blink slowly
   if(receiveDmxTimeout > 0){
+
     receiveDmxTimeout --;
-    receiveDmxBlinkTimer ++;
-  
-  digitalWrite(6,HIGH);
-    if(receiveDmxBlinkTimer > 500){
-//      digitalWrite(6,HIGH);
+    receiveDmxBlinkTimer --;
+
+    if(receiveDmxBlinkTimer < 0){
+      if(blinkState){
+        blinkState = false;
+      } 
+      else {
+        blinkState = true;
+      }
+      digitalWrite(ledPin,blinkState);   
+      if(fastBlink){
+        receiveDmxBlinkTimer = 12000;  
+      } 
+      else { 
+        receiveDmxBlinkTimer = 32000;  
+      }
     } 
-    else if(receiveDmxBlinkTimer > 1000) {
-    //  analogWrite(6,ledBaseValue);
- //   digitalWrite(6,LOW);
-      receiveDmxBlinkTimer = 0;
-    }
-  } else {
-      analogWrite(6,50);
+  } 
+  else {
+    blinkState = true;
+    digitalWrite(ledPin,blinkState);
   }
 } //end loop()
 
@@ -193,11 +197,18 @@ ISR(USART_RX_vect){
   if(dmxcurrent > dmxaddress) {         //check if the current address is the one we want.
     dmxvalue[i] = dmxreceived;
     i++;
+     //zuuuuuuuuu6aaaaaaaauaaaaaauuxsuxssssuxus$$$$$$$$$jx1jiiiiiiiiiiiii0 mySerial.println(i);
     if(i == NUMBER_OF_CHANNELS) {
       bitClear(UCSR0B, RXCIE0);
       dmxnewvalue = 1;                        //set newvalue, so that the main code can be executed.
     }
   }
 } // end ISR
+
+
+
+
+
+
 
 
